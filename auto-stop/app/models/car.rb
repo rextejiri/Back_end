ac7222767c8baf746cf5e77537220f3e2b27b1d9
rextejiri <rextejiri@gmail.com>
2021-01-31 07:28:47 -0600

    index route shows all cars

--- auto-stop/app/models/car.rb
+++ auto-stop/app/models/car.rb
@@ -1,5 +1,15 @@
 class Car < ApplicationRecord
+  DB = PG.connect({:host => "localhost", :port => 5432, :dbname => 'auto_stop_development'})
   def self.all
-    { name: "joe", car: "benz"}
+    results = DB.exec("SELECT * FROM cars;")
+    return results.map do |result|
+      {
+        "id" => result["id"].to_i,
+        "make" => result["make"],
+        "type" => result["type"],
+        "model" => result["model"],
+        "manufacturer_id" => result["manufacturer_id"].to_i
+      }
+    end
   end
 end
