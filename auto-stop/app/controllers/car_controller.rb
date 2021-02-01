5ba95374ace94cf9dea503d490e6315d402f744d
rextejiri <rextejiri@gmail.com>
2021-02-01 07:22:55 -0600

    create route works

--- auto-stop/app/controllers/car_controller.rb
+++ auto-stop/app/controllers/car_controller.rb
@@ -1,8 +1,16 @@
 class CarController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render json: Car.all
   end
+
   def show
     render json: Car.find(params["id"])
   end
+
+  def create
+    render json: Car.create(params["cars"])
+  end
+
 end
